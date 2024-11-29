5c28f38834650e1482a0783fae9f984162e3f14e
NTiger07 <falaleru@gmail.com>
2024-11-29 17:59:10 +0100

    Initial Commit

---
+++ src/main/java/com/rtsynchdoc/rtsynchdoc/RtsynchdocApplication.java
@@ -0,0 +1,13 @@
+package com.rtsynchdoc.rtsynchdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtsynchdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtsynchdocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtsynchdoc/rtsynchdoc/RtsynchdocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtsynchdoc.rtsynchdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtsynchdocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
